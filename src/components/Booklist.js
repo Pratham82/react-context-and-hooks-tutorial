80758c641905d4c7d8727d18148f1dd21c9e3f00
Pratham82 <mali.prathamesh82@gmail.com>
2020-10-21 17:46:41 +0530

    Using context with 1st method

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
-
+import { ThemeContext } from "../contexts/ThemeContext";
+import contextType from "react";
 class Booklist extends Component {
   state = {};
+  static contextType = ThemeContext;
+
   render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+
     return (
-      <div className="book-list">
+      <div
+        className="book-list"
+        style={{ background: theme.bg, color: theme.syntax }}
+      >
         <ul>
-          <li>Deep work</li>
-          <li>Ikagai</li>
-          <li>Grit</li>
-          <li>How not to Die</li>
-          <li>Principles</li>
+          <li style={{ background: theme.ui }}>Deep work - Cal Newport</li>
+          <li style={{ background: theme.ui }}>Ikagai- Hector garcia</li>
+          <li style={{ background: theme.ui }}>Grit - Angela Duckworth</li>
+          <li style={{ background: theme.ui }}>
+            How not to Die - Michael Gregger
+          </li>
+          <li style={{ background: theme.ui }}>Principles - Ray Dalio</li>
         </ul>
       </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
-const Navbar = () => (
-  <nav>
-    <h1>Reading List</h1>
-    <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
-  </nav>
-);
+class Navbar extends React.Component {
+  static contextType = ThemeContext;
+  render() {
+    console.log(this.context);
+    const { dark, light, isLightTheme } = this.context;
+    const theme = isLightTheme ? light : dark;
+    console.log(theme);
 
+    return (
+      <nav style={{ background: theme.ui, color: theme.syntax }}>
+        <h1>Reading List</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    );
+  }
+}
 export default Navbar;
