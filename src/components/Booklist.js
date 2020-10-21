2b6c30cbfdf58b93b62d2d2794cb3f8631f2a60d
Pratham82 <mali.prathamesh82@gmail.com>
2020-10-21 18:21:42 +0530

    Using context with 2nd method i.e ContextConsumeR

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -2,28 +2,35 @@ import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 import contextType from "react";
 class Booklist extends Component {
-  state = {};
-  static contextType = ThemeContext;
-
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-
     return (
-      <div
-        className="book-list"
-        style={{ background: theme.bg, color: theme.syntax }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>Deep work - Cal Newport</li>
-          <li style={{ background: theme.ui }}>Ikagai- Hector garcia</li>
-          <li style={{ background: theme.ui }}>Grit - Angela Duckworth</li>
-          <li style={{ background: theme.ui }}>
-            How not to Die - Michael Gregger
-          </li>
-          <li style={{ background: theme.ui }}>Principles - Ray Dalio</li>
-        </ul>
-      </div>
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+
+          return (
+            <div
+              className="book-list"
+              style={{ background: theme.bg, color: theme.syntax }}
+            >
+              <ul>
+                <li style={{ background: theme.ui }}>
+                  Deep work - Cal Newport
+                </li>
+                <li style={{ background: theme.ui }}>Ikagai- Hector garcia</li>
+                <li style={{ background: theme.ui }}>
+                  Grit - Angela Duckworth
+                </li>
+                <li style={{ background: theme.ui }}>
+                  How not to Die - Michael Gregger
+                </li>
+                <li style={{ background: theme.ui }}>Principles - Ray Dalio</li>
+              </ul>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
