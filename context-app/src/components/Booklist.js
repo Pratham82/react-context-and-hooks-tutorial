9300b1870b9be3ca7976cbeeb542d1d5f53573d1
Pratham82 <mali.prathamesh82@gmail.com>
2020-10-22 17:59:06 +0530

    Converted to functional component and Added context

--- context-app/src/components/Booklist.js
+++ context-app/src/components/Booklist.js
@@ -1,38 +1,26 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
-import contextType from "react";
-class Booklist extends Component {
-  render() {
-    return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, light, dark } = context;
-          const theme = isLightTheme ? light : dark;
 
-          return (
-            <div
-              className="book-list"
-              style={{ background: theme.bg, color: theme.syntax }}
-            >
-              <ul>
-                <li style={{ background: theme.ui }}>
-                  Deep work - Cal Newport
-                </li>
-                <li style={{ background: theme.ui }}>Ikagai- Hector garcia</li>
-                <li style={{ background: theme.ui }}>
-                  Grit - Angela Duckworth
-                </li>
-                <li style={{ background: theme.ui }}>
-                  How not to Die - Michael Gregger
-                </li>
-                <li style={{ background: theme.ui }}>Principles - Ray Dalio</li>
-              </ul>
-            </div>
-          );
-        }}
-      </ThemeContext.Consumer>
-    );
-  }
-}
+const Booklist = () => {
+  const { isLightTheme, dark, light } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+
+  return (
+    <div
+      className="book-list"
+      style={{ background: theme.bg, color: theme.syntax }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>Deep work - Cal Newport</li>
+        <li style={{ background: theme.ui }}>Ikagai- Hector garcia</li>
+        <li style={{ background: theme.ui }}>Grit - Angela Duckworth</li>
+        <li style={{ background: theme.ui }}>
+          How not to Die - Michael Gregger
+        </li>
+        <li style={{ background: theme.ui }}>Principles - Ray Dalio</li>
+      </ul>
+    </div>
+  );
+};
 
 export default Booklist;
