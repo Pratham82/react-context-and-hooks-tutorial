d974f5b343d81c46e81cbcc7c7d52dafb073a116
Pratham82 <mali.prathamesh82@gmail.com>
2020-10-22 17:06:15 +0530

    Added useEffect hook

--- hooks-app/src/components/SongsList.js
+++ hooks-app/src/components/SongsList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { v4 as uuid } from "uuid";
 import NewSongForm from "./NewSongForm";
 
@@ -9,8 +9,21 @@ const SongsList = () => {
     { title: "New Light", artist: "John Mayer", id: 3 },
   ]);
 
+  const [age, setAge] = useState(18);
+
   const addSong = (title, artist) =>
     setSongs([...songsList, { title, artist, id: uuid() }]);
+
+  // 1st parameter: is the cleanup funciton which will run after the component update
+  // 2nd parameter: We can specify the component, so useEffect will focus on that specific function instead of any change on the dom
+  useEffect(() => {
+    // If you are using the state inside the callback function then you have to add that state to the dependency array inside the useEffect function
+    console.log("useEffect  ran", songsList);
+  }, [songsList]);
+
+  useEffect(() => {
+    console.log("useEffect  ran", age);
+  }, [age]);
 
   return (
     <div className="song-list">
@@ -23,6 +36,8 @@ const SongsList = () => {
         ))}
       </ul>
       <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>^ Increase Age</button>
+      <h3>Current Age: {age}</h3>
     </div>
   );
 };
