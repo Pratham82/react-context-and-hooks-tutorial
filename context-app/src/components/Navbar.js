adb9aa1fabff5bd81423b924382a7484521ebe9f
Pratham82 <mali.prathamesh82@gmail.com>
2020-10-22 18:11:44 +0530

    Added multiple contexts using useContext hook

--- context-app/src/components/Navbar.js
+++ context-app/src/components/Navbar.js
@@ -1,43 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 import { AuthContext } from "../contexts/AuthContext";
 
-class Navbar extends React.Component {
-  render() {
-    return (
-      <AuthContext.Consumer>
-        {(authContext) => (
-          <ThemeContext.Consumer>
-            {(themeContext) => {
-              const { dark, light, isLightTheme } = themeContext;
-              const theme = isLightTheme ? light : dark;
-              const { isAuthenticated, toggleAuth } = authContext;
+const Navbar = () => {
+  const { isLightTheme, dark, light } = useContext(ThemeContext);
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+  const theme = isLightTheme ? light : dark;
 
-              return (
-                <nav style={{ background: theme.ui, color: theme.syntax }}>
-                  <h1>Reading List</h1>
-                  <div>
-                    {" "}
-                    <h2 onClick={toggleAuth}>
-                      User {isAuthenticated ? "Logged IN" : "Logged OUT"}
-                    </h2>
-                    <button onClick={toggleAuth}>
-                      {" "}
-                      {isAuthenticated ? "Log Out" : "Log In"}
-                    </button>{" "}
-                  </div>
-                  <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                  </ul>
-                </nav>
-              );
-            }}
-          </ThemeContext.Consumer>
-        )}
-      </AuthContext.Consumer>
-    );
-  }
-}
+  return (
+    <nav style={{ background: theme.ui, color: theme.syntax }}>
+      <h1>Reading List</h1>
+      <div>
+        {" "}
+        <h2 onClick={toggleAuth}>
+          User {isAuthenticated ? "Logged IN" : "Logged OUT"}
+        </h2>
+        <button onClick={toggleAuth}>
+          {" "}
+          {isAuthenticated ? "Log Out" : "Log In"}
+        </button>{" "}
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
 export default Navbar;
