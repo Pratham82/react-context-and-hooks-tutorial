400a43859edf8555e8e2f1d9cea9e5fb300bc9f5
Pratham82 <mali.prathamesh82@gmail.com>
2020-10-23 15:58:23 +0530

    Added BookContextProvider

--- context-app/src/App.js
+++ context-app/src/App.js
@@ -1,20 +1,23 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Booklist from "./components/Booklist";
-import ThemeToggle from "./components/ThemeToggle";
-import ThemeContextProvider from "./contexts/ThemeContext";
-import AuthContextProvider from "./contexts/AuthContext";
+import React from "react"
+import Navbar from "./components/Navbar"
+import Booklist from "./components/Booklist"
+import ThemeToggle from "./components/ThemeToggle"
+import ThemeContextProvider from "./contexts/ThemeContext"
+import AuthContextProvider from "./contexts/AuthContext"
+import BookContextProvider from "./contexts/BookContext"
 
 const App = () => (
   <div className="App">
     <AuthContextProvider>
       <ThemeContextProvider>
         <Navbar />
-        <Booklist />
+        <BookContextProvider>
+          <Booklist />
+        </BookContextProvider>
         <ThemeToggle />
       </ThemeContextProvider>
     </AuthContextProvider>
   </div>
-);
+)
 
-export default App;
+export default App
